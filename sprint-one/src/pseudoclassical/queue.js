0b08ed26b825b865d11d5edfdd2307078586e26c
realmike33 <realmike33@gmail.com>
2014-12-10 16:26:12 -0800

    Completed Pseudoclassical Queue and Stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,4 +3,24 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Queue.prototype.enqueue = function(value){
+    this.storage[this.indexBack] = value;
+    this.indexBack++;
+};
 
+Queue.prototype.dequeue = function(){
+    if(this.size() > 0){
+      var current = this.storage[this.indexFront];
+      delete this.storage[this.indexFront];
+      this.indexFront++;
+      return current;
+    }
+};
+
+Queue.prototype.size = function(){
+    return this.indexBack - this.indexFront;
+};
+
+Queue.prototype.storage = {};
+Queue.prototype.indexFront = 0;
+Queue.prototype.indexBack = 0;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,4 +3,22 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.storage = {};
+Stack.prototype.indexTop = 0;
+Stack.prototype.push = function(value){
+    this.storage[this.indexTop] = value;
+    this.indexTop++;
+  };
+Stack.prototype.pop = function(){
+    if(this.size() > 0){
+      var index = this.indexTop - 1;
+      var current = this.storage[index];
+      delete this.storage[index];
+      this.indexTop--;
+      return current;
+    }
+  };
+Stack.prototype.size = function(){
+    return this.indexTop;
+  };
 
