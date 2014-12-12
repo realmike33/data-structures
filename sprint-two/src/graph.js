c3101366a0aeb0380ac26424dcb1e99dcdffb665
realmike33 <realmike33@gmail.com>
2014-12-11 20:06:56 -0800

    Started on Graphs, completed a few methods.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,13 +3,23 @@
 var Graph = function(){
 };
 
+Graph.prototype.nodes = {};
+
 Graph.prototype.addNode = function(node){
+  var key = JSON.stringify(node);
+  this.nodes[key] = {value: node, connections: []};
 };
 
 Graph.prototype.contains = function(node){
+  var key = JSON.stringify(node);
+  return !!this.nodes[key];
 };
 
 Graph.prototype.removeNode = function(node){
+  var key = JSON.stringify(node);
+  if (this.nodes[key]) {
+    delete this.nodes[key];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
