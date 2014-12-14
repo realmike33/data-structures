919541eebf2b89d6feff931037c81c3d66ec464c
realmike33 <realmike33@gmail.com>
2014-12-13 16:40:34 -0800

    Starting on hashTable extra credit

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,11 +1,44 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
 //~O(1)
+
+HashTable.prototype.checkLimit = function(){
+  console.log('This is the size ', this._size);
+  console.log('This is the limit', this._limit);
+  if (this._size / this._limit >= 0.75) {
+    this._limit *= 2;
+
+
+    var tuples = [];
+    this._storage.each(function(bucket){
+
+      if (bucket) {
+        for(var i = 0; i < bucket.length; i++){
+          tuples.push(bucket[i]);
+        }
+      }
+    });
+
+    this._storage = LimitedArray(this._limit);
+
+    this._size = 0;
+    tuples.forEach((function(tuple) {
+      this.insert(tuple[0], tuple[1]);
+    }).bind(this));
+
+  } else if (this._limit / this._size < 0.25) {
+
+  }
+};
+
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var valueInSlot = this._storage.get(i);
+  this._size++;
 
   if (!valueInSlot) {
     this._storage.set(i, []);
@@ -24,6 +57,9 @@ HashTable.prototype.insert = function(k, v){
   if (!containsKey) {
     valueInSlot.push([k, v]);
   }
+
+  this.checkLimit();
+
 };
 //~O(1)
 HashTable.prototype.retrieve = function(k){
